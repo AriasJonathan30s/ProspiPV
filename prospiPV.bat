7e6515a1852706304ccb30ec99600b1f18ddd3bb
AriasJonathan30s <jacarijon@gmail.com>
2024-11-30 02:12:58 -0600

    fix commands

--- prospiPV.bat
+++ prospiPV.bat
@@ -1,8 +1,7 @@
 @echo off
 TITLE Prospi Punto de Venta
 
-git diff --quiet || git stash && git pull origin master
-npm i --save
+git diff --quiet || (git stash && git pull origin master && npm i --save)
 
 start ng serve --configuration=production --public-host -o
 exit
