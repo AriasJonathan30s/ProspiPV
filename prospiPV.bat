34821085985ac2abeda091e6d83691ce3bad77b8
AriasJonathan30s <jacarijon@gmail.com>
2024-11-30 02:41:53 -0600

    fix

--- prospiPV.bat
+++ prospiPV.bat
@@ -1,7 +1,7 @@
 @echo off
 TITLE Prospi Punto de Venta
 
-git diff --quiet || (git stash && git pull force origin master && npm i --save)
+git diff --quiet || (git stash && git pull && npm i --save)
 
 npm start
 exit
